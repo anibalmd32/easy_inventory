5f6a12135aef3af64e633fafacacb72712370c66
Anibal Mendoza <anibalmd32@gmail>
2024-06-30 18:41:48 -0400

    refactor: modern css

--- src/definitions.d.ts
+++ src/definitions.d.ts
@@ -2,3 +2,4 @@ export type IconType = (props: SVGProps<SVGSVGElement>) => JSXNode;
 export type ComponentSize = 'sm' | 'md' | 'lg';
 export type ComponentVariant = 'solid' | 'subtle' | 'outline';
 export type ButtonType = 'button' | 'submit' | 'reset';
+export type ComponentTheme = 'dark' | 'light';
