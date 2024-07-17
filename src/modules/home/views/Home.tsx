e2169b75e6cbba9430fb6a1c4429751adf18a8c3
Anibal Mendoza <anibalmd32@gmail>
2024-07-17 11:29:42 -0400

    style scroll bar

--- src/modules/home/views/Home.tsx
+++ src/modules/home/views/Home.tsx
@@ -14,12 +14,6 @@ export const Home = component$(() => {
             </article>
             <article class='last-invoices'>
                 <p>Ultimas facturas</p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia architecto hic consectetur ipsum accusantium autem debitis fugiat vel, ipsam veniam quam sunt aut reprehenderit quae facilis quia nihil! Nisi, corporis!
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis non perspiciatis itaque magnam doloribus nobis commodi iure modi vero quam quibusdam neque mollitia quaerat hic, beatae esse odio aperiam pariatur.
-                </p>
             </article>
             <article class='stock-alert'>
                 <p>Alerta de stock</p>
