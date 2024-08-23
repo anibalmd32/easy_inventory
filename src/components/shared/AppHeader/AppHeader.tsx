2124f1e01f8913176cc94bdaf19fa685379ed1df
Anibal Mendoza <anibalmd32@gmail>
2024-08-22 23:12:03 -0400

    fix header spaces

--- src/components/shared/AppHeader/AppHeader.tsx
+++ src/components/shared/AppHeader/AppHeader.tsx
@@ -8,11 +8,11 @@ export interface AppHeaderProps {
 
 export function AppHeader(props: AppHeaderProps) {
 	return (
-		<header className="w-full fixed top-0 flex justify-between items-center py-2 bg-gray-950 text-gray-200">
-			<div className='md:container md:mx-auto flex items-center justify-between'>
+		<header className="w-full fixed top-0 flex gap-2 justify-between items-center p-2 bg-gray-950 text-gray-200">
+			<div className='md:container md:mx-auto flex items-center justify-between flex-1'>
 				<div className="flex items-center">
 					<NextImage src={props.appLogo} alt="logo" className="w-8 h-8" width={40} height={40} />
-					<span className="text-xl font-bold ml-2">{props.appName}</span>
+					<span className="text-sm md:text-xl font-bold ml-2">{props.appName}</span>
 				</div>
 
 				<HeaderMenu />
--- src/components/shared/AppHeader/HeaderMenu.tsx
+++ src/components/shared/AppHeader/HeaderMenu.tsx
@@ -5,26 +5,35 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
 import { Button } from '@/components/ui/button';
+import { AlignJustify } from 'lucide-react';
 
 export function HeaderMenu() {
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
-				<Button className="bg-gray-900 hover:bg-gray-800 text-gray-200">
-					Operaciones
-				</Button>
+				<div>
+					{/* Icono para Mobile */}
+					<Button className="bg-gray-900 hover:bg-gray-800 text-gray-200 p-2 md:hidden">
+						<AlignJustify className="w-6 h-6" />
+					</Button>
+
+					{/* Botón para Desktop */}
+					<Button className="bg-gray-900 hover:bg-gray-800 text-gray-200 hidden md:block">
+						Operaciones
+					</Button>
+				</div>
 			</DropdownMenuTrigger>
 			<DropdownMenuContent className="bg-gray-950 text-gray-200">
 				<DropdownMenuLabel>Empresa</DropdownMenuLabel>
-				<DropdownMenuItem className="cursor-pointer">Facturacion</DropdownMenuItem>
+				<DropdownMenuItem className="cursor-pointer">Facturación</DropdownMenuItem>
 				<DropdownMenuItem className="cursor-pointer">Lista de compras</DropdownMenuItem>
 				<DropdownMenuItem className="cursor-pointer">Reportes</DropdownMenuItem>
 
 				<DropdownMenuSeparator />
-				<DropdownMenuLabel>Aplicacion</DropdownMenuLabel>
-				<DropdownMenuItem className="cursor-pointer">Cerrar Sesion</DropdownMenuItem>
+				<DropdownMenuLabel>Aplicación</DropdownMenuLabel>
+				<DropdownMenuItem className="cursor-pointer">Cerrar Sesión</DropdownMenuItem>
 			</DropdownMenuContent>
 		</DropdownMenu>
 	);
