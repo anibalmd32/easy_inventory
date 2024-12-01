28d26a93ab2b7ccfbf97a2ba6d0396b50c14b622
Anibal Mendoza <anibalmd32@gmail.com>
2024-12-01 08:53:50 -0400

    refactor form styles

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -14,7 +14,10 @@ export default function AuthPage() {
 
   const handleSubmit = async (event: any) => {
     event.preventDefault();
+
     setLoading(true);
+    setErrorMessage(null);
+
     const formData = new FormData(event.target);
     const response = await signIn('credentials', {
       email: formData.get('email'),
@@ -50,51 +53,53 @@ export default function AuthPage() {
           Inversiones Jeicar, C.A.
         </h1>
       </div>
-      <form className="w-[400px] mx-auto" onSubmit={handleSubmit}>
-        <div className="mb-5">
-          <label
-            htmlFor="email"
-            className="block mb-2 text-sm font-medium text-gray-200 dark:text-white w-full"
-          >
-            Tu correo
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="name@flowbite.com"
-            required
-          />
-        </div>
-        <div className="mb-5">
-          <label
-            htmlFor="password"
-            className="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
-          >
-            Tu contraseña
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required
-          />
-        </div>
-        <div className="mb-5 text-red-200">
-          {errorMessage && <p>{errorMessage}</p>}
-        </div>
-        <div className="flex justify-center gap-2">
-          <button
-            type="submit"
-            className="text-white bg-gray-900 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mx-auto flex items-center gap-2"
-          >
-            <span>Iniciar sesión</span>
-            {loading && <Spinner />}
-          </button>
-        </div>
-      </form>
+      <div className="w-[500px] mx-auto bg-gray-900 rounded-lg px-5 py-8 flex flex-col gap-5">
+        <form className="w-[400px] mx-auto" onSubmit={handleSubmit}>
+          <div className="mb-5">
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-200 dark:text-white w-full"
+            >
+              Tu correo
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="nombre@inventario.com"
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
+            >
+              Tu contraseña
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+            />
+          </div>
+          <div className="mb-5 text-red-200">
+            {errorMessage && <p>{errorMessage}</p>}
+          </div>
+          <div className="flex justify-center gap-2">
+            <button
+              type="submit"
+              className="text-white bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mx-auto flex items-center gap-2"
+            >
+              <span>Iniciar sesión</span>
+              {loading && <Spinner />}
+            </button>
+          </div>
+        </form>
+      </div>
 
       <ProgressBar
         height="4px"
